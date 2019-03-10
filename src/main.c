7df145315664e58de65e1c4af11d225a5388437f
SimonFredrich <21simplex02@gmail.com>
2019-03-10 15:13:56 +0100

    typo, userinterface with switch statement, homework output format, multiple word scanf

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ const char fileLocation[] = "/home/simplex/Documents/studentSchedule/storage/hom
 struct homework {
     int id;
     char title[30];
-    char desciption[100];
+    char descritiption[100];
 };
 
 void saveHomework(struct homework detailsWrite);
@@ -17,8 +17,27 @@ void userRead();
 
 int main(int argc, char **argv) {
 
-    userRead();
+    char decision;
+    int decisionBool;
 
+    printf("Insert homework (i) or view homework (v): ");
+    scanf(" %c", &decision);
+
+
+    switch (decision) {
+        case 105:
+            userWrite();
+            decisionBool = 0;
+            break;
+        case 118:
+            userRead();
+            decisionBool = 0;
+            break;
+        default:
+            printf("\nYou did something wrong.\n\n");
+            decisionBool = 1;
+            break;
+    }
     return 0;
 }
 
@@ -27,7 +46,7 @@ void saveHomework(struct homework detailsWrite) {
 
     printf("%d\n", detailsWrite.id);
     printf("%s\n", detailsWrite.title);
-    printf("%s\n", detailsWrite.desciption);
+    printf("%s\n", detailsWrite.descritiption);
 
     pfile = fopen(fileLocation, "ab");
 
@@ -41,6 +60,11 @@ void displayAllHomework(struct homework detailsRead) {
 
     pfile = fopen(fileLocation, "rb");
 
+    system("clear");
+
+    printf("\nHomework:\n\n\n");
+    printf("id:\t\ttitle:\t\tdescritiption:\n\n");
+
     while(1) {
         fread(&detailsRead, sizeof(detailsRead), 1, pfile);
 
@@ -48,9 +72,10 @@ void displayAllHomework(struct homework detailsRead) {
             break;
         }
 
-        printf("%d\n", detailsRead.id);
-        printf("%s\n", detailsRead.title);
-        printf("%s\n", detailsRead.desciption);
+        printf("%d\t\t", detailsRead.id);
+        printf("%s\t\t", detailsRead.title);
+        printf("%s\n", detailsRead.descritiption);
+        printf("-------------------------------------------------------------------------\n");
     }
     fclose(pfile);
 }
@@ -58,14 +83,14 @@ void displayAllHomework(struct homework detailsRead) {
 void userWrite() {
     struct homework detailsWrite;
 
-    printf("Enter homework id: ");
+    printf("Enter homework id (int): ");
     scanf(" %d", &detailsWrite.id);
 
-    printf("Enter homework title: ");
-    scanf(" %s", detailsWrite.title);
+    printf("Enter homework title (string): ");
+    scanf(" %[^\n]s", detailsWrite.title);
 
-    printf("Enter homework desciption: ");
-    scanf(" %s", detailsWrite.desciption);
+    printf("Enter homework descritiption (string): ");
+    scanf(" %[^\n]s", detailsWrite.descritiption);
 
     saveHomework(detailsWrite);
 }
